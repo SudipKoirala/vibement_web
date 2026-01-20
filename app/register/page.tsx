91b6a2d4b0d3707f2cc0551845497fd845e8f2ea
Sudip Koirala <soodeep321@gmail.com>
2026-01-20 15:42:57 +0545

    update

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,4 +1,4 @@
-import Layout from "@/components/Layout";
+import Layout from "../../components/Layout";
 import RegisterForm from "../../components/RegisterForm";
 
 export default function RegisterPage() {
